93511b6ea32aef7ef0ef07f528a7aab425a8f799
rylderoliveira <rylder2014@gmail.com>
2023-11-05 21:39:51 -0300

    solve 28

---
+++ src/main/kotlin/easy/28FindIndex.kt
@@ -0,0 +1,23 @@
+package easy
+
+fun strStr(haystack: String, needle: String): Int {
+    if (needle.isBlank()) {
+        return -1
+    }
+
+    if (haystack.contains(needle).not()) {
+        return -1
+    }
+    var word = ""
+    haystack.forEachIndexed { index, c ->
+        word += c
+        if (word.contains(needle)) {
+            return index + 1 - needle.length
+        }
+    }
+    return -1
+}
+
+fun main() {
+    strStr("hello", "ll")
+}
