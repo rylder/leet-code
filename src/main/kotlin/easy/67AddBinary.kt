75e350f7afff2fc2644583e3bbaa63c1f50e1600
rylder <rylder2014@gmail.com>
2024-08-03 11:18:04 -0300

    feat: add 67 add binary

--- src/main/kotlin/easy/67AddBinary.kt
+++ src/main/kotlin/easy/67AddBinary.kt
@@ -1,15 +1,28 @@
 package easy
 
 fun addBinary(a: String, b: String): String {
-    var result = ""
+    var carry = 0
+    val result = StringBuilder()
 
+    val maxLength = maxOf(a.length, b.length)
 
+    for (i in 0 until maxLength) {
+        val bitA = if (i < a.length) a[a.length - 1 - i] - '0' else 0
+        val bitB = if (i < b.length) b[b.length - 1 - i] - '0' else 0
+        val sum = bitA + bitB + carry
+        result.append(sum % 2)
+        carry = sum / 2
+    }
 
-    return result
+    if (carry != 0) {
+        result.append(carry)
+    }
+
+    return result.reverse().toString()
 }
 
 fun main() {
-    val a = "11"
+    val a = "110"
     val b = "1"
     println(addBinary(a, b))
 }
